55a10b301b0d15a5339f0bf796cba7a3455575ee
5qr1 <eracrit@proton.me>
2024-12-15 11:28:54 +0000

    android support TEST

--- scripts/mobileKeyboardButton.js
+++ scripts/mobileKeyboardButton.js
@@ -1,10 +1,14 @@
 export function showMobileButtonIfNeeded() {
-    const isMobile = /iPhone|iPad|iPod/i.test(navigator.userAgent) && !/Macintosh/i.test(navigator.userAgent);
+    const isMobile = (() => {
+        const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+        const isAndroid = /android/i.test(userAgent);
+        const isIOS = /iPhone|iPad|iPod/i.test(userAgent) && !/Macintosh/i.test(userAgent);
+        const isMobileHint = /mobile/i.test(userAgent);
+        return isAndroid || isIOS || isMobileHint;
+    })();
     const mobileKeyboardButton = document.getElementById('mobile-keyboard-button');
-
     if (isMobile) {
         mobileKeyboardButton.style.display = 'block';
-
         mobileKeyboardButton.addEventListener('click', () => {
             const input = document.createElement('input');
             input.type = 'text';
@@ -13,10 +17,8 @@ export function showMobileButtonIfNeeded() {
             input.style.left = '-9999px';
             input.style.height = '1px';
             input.style.width = '1px';
-
             document.body.appendChild(input);
             input.focus();
-
             input.addEventListener('blur', () => {
                 document.body.removeChild(input);
             });
